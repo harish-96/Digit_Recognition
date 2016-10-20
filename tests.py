b189a2981e2e9bd52ba2d7050a5ebf4311fd563e
= <=>
2016-10-20 18:33:31 +0530

    Added test functions to check that the functions in the neural network takes the correct inputs

--- tests.py
+++ tests.py
@@ -1,6 +1,34 @@
 import unittest
+import math
+import numpy as np
+from text_recog import *
+
+
 class TestNeuralNetwork(unittest.TestCase):
 
-    def test_
+    def setUp(self):
+        X_train, y_train = load_data("./traindata.mat")
+        self.nn = NN_hwr([len(X_train[0]), 30, 10])
+
+    def test_NN_hwr_raises_exception_for_non_numeric_values(self):
+        self.assertRaises(TypeError, NN_hwr, ["sb", "uir", 5])
+
+    def test_sigmoid_function_returns_correct_value(self):
+        self.assertAlmostEqual(sigmoid(0), 0.5)
+        self.assertAlmostEqual(sigmoid(1), 1 / (1 + e**-1))
+        self.assertListEqual(sigmoid([0, 0, 0], np.array([0.5, 0.5, 0.5])))
+
+    def test_sigmoid_derivative_returns_correct_value(self):
+        self.assertAlmostEqual(sigmoid_derivative(0), 0.25)
+        self.assertAlmostEqual(sigmoid(1), 0.19661193324)
+        self.assertAlmostEqual(sigmoid(36), 0.0)
+        self.assertAlmostEqual(sigmoid(-36), 0.0)
+
+    def test_back_prop_rejects_inputs_of_incorrect_sizes(self):
+        self.assertRaises(TypeError, nn.back_prop, [np.zeros((782, 1)), np.zeros((1, 10))])
+        self.assertRaises(TypeError, nn.back_prop, ["abc", "def"])
+
+
+
 if __name__ == '__main__':
     unittest.main()
