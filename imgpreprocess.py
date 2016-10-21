a648f0491d336d38b815cde30248eb76d9710467
Surya Mohan <suryamohan@iitb.ac.in>
2016-10-21 07:07:53 +0530

    Fixed infinite loop error in segment_characters() and added docstrings

--- imgpreprocess.py
+++ imgpreprocess.py
@@ -1,10 +1,22 @@
+"""
+imgpreprocess.py
+~~~~~~~~~~~~~~~~
+A module to preprocess and segement an image containing text 
+into lines , words and characters.And also change resolution of 
+the segmented characters to the resolution required by the NeuralNet.py 
+to recognise the characters.
+"""
+
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-class Segmentation(object):
+class Preprocess(object):
     def __init__(self,imagepath):
+	"""imagepath is the address of the image that needs to be preprocessed"""
         self.image = cv2.imread(str(imagepath),0)
     def binaryimg(self,image):
+	"""Converts grayscale image to binary image , it takes 1 for black and it takes zero for white """
         blur_image = cv2.GaussianBlur(image,(5,5),0)
         retval,binary_image = cv2.threshold(blur_image,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         m, n = binary_image.shape
@@ -19,6 +31,7 @@ class Segmentation(object):
                     binary_image[j,i]=0
         return binary_image
     def cropimg(self,image):
+	"""Crops a binary image tightly"""
         m,n = image.shape
         c1 = n 
         c2 = 0
@@ -37,6 +50,7 @@ class Segmentation(object):
                         r2 = j
         return image[r1:r2+1,c1:c2+1]
     def segment_lines(self):
+	"""The image containing text is segmented into lines and returns a list of the lines""" 
         cropped_image = self.cropimg(self.binaryimg(self.image))
         lines = []
         limg = cropped_image.copy()
@@ -50,25 +64,19 @@ class Segmentation(object):
                     p = i
                     break
             limg = limg[p:,:]
-            limg = self.trim_top(limg)
+            limg = self.cropimg(limg)
         lines.append(last_line)
         return lines
     def last_line(self,img):
+	"""Takes cropped binary image as input and gives the segment of image containing last line of text""" 
         m,n = img.shape
         for i in list(reversed(range(m-1))):
             if np.sum(img[i,:])==0 and np.sum(img[i+1,:])!=0:                
                 last_line = img[i+1:,:]
                 break
         return last_line
-    def trim_top(self,img):
-        m,n = img.shape
-        r = m
-        for i in range(n):
-            for j in range(m):
-                if img[j,i]==1 and j < r:
-                    r =j
-        return img[r:,:]
     def segment_characters(self,line):
+	"""Takes a line from a segemented image and returns a list of characters in the line"""
         line = self.cropimg(line)
         chars = []
         cimg = line.copy()
@@ -82,22 +90,16 @@ class Segmentation(object):
                     p = i
                     break
             cimg = cimg[:,p:]
-            cimg = self.trim_left(cimg)
+            cimg = self.cropimg(cimg)
         chars.append(last_char)
         return chars
     def last_char(self,img):
+	"""Takes a line from a segemented image and returns last character in the line"""
         m,n = img.shape
         for i in list(reversed(range(n-1))):
             if np.sum(img[:,i])==0 and np.sum(img[:,i+1])!=0:                
                 last_char = img[:,i+1:]
                 break
         return last_char
-    def trim_left(self,img):
-        m,n = img.shape
-        c = n
-        for i in range(n):
-            for j in range(m):
-                if img[j,i]==1 and i < c:
-                    c = i
-        return img[c:,:]
+
 
