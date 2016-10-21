179ffd6f52f8c09870aa53943003d4d0b3e7d2c2
Surya Mohan <suryamohan@iitb.ac.in>
2016-10-21 06:22:24 +0530

    Segmentation of line into characters added , but infinite loop error in segment_characters()

--- imgpreprocess.py
+++ imgpreprocess.py
@@ -19,15 +19,14 @@ class Segmentation(object):
                     binary_image[j,i]=0
         return binary_image
     def cropimg(self,image):
-        binary_image = self.binaryimg(image)
-        m,n = binary_image.shape
+        m,n = image.shape
         c1 = n 
         c2 = 0
         r1 = m
         r2 = 0
         for i in range(n):
             for j in range(m):
-                if binary_image[j,i]==1: 
+                if image[j,i]==1: 
                     if i < c1:
                         c1 =i
                     if i > c2:
@@ -36,12 +35,11 @@ class Segmentation(object):
                         r1 = j
                     if  j > r2 :
                         r2 = j
-        return binary_image[r1:r2+1,c1:c2+1]
+        return image[r1:r2+1,c1:c2+1]
     def segment_lines(self):
-        cropped_image = self.cropimg(self.image)
+        cropped_image = self.cropimg(self.binaryimg(self.image))
         lines = []
         limg = cropped_image.copy()
-        
         last_line = self.last_line(limg)
         while not np.sum(limg) == np.sum(last_line):
             m,n = limg.shape
@@ -64,11 +62,42 @@ class Segmentation(object):
         return last_line
     def trim_top(self,img):
         m,n = img.shape
+        r = m
+        for i in range(n):
+            for j in range(m):
+                if img[j,i]==1 and j < r:
+                    r =j
+        return img[r:,:]
+    def segment_characters(self,line):
+        line = self.cropimg(line)
+        chars = []
+        cimg = line.copy()
+        last_char = self.last_char(cimg)
+        while not np.sum(cimg) == np.sum(last_char):
+            m,n = cimg.shape
+            p = 0
+            for i in range(1,n):
+                if np.sum(cimg[:,i])==0 and np.sum(cimg[:,i-1])!=0:
+                    chars.append(cimg[:,:i])
+                    p = i
+                    break
+            cimg = cimg[:,p:]
+            cimg = self.trim_left(cimg)
+        chars.append(last_char)
+        return chars
+    def last_char(self,img):
+        m,n = img.shape
+        for i in list(reversed(range(n-1))):
+            if np.sum(img[:,i])==0 and np.sum(img[:,i+1])!=0:                
+                last_char = img[:,i+1:]
+                break
+        return last_char
+    def trim_left(self,img):
+        m,n = img.shape
         c = n
         for i in range(n):
             for j in range(m):
                 if img[j,i]==1 and i < c:
-                    c =i
-        return img[:,c:]
-
+                    c = i
+        return img[c:,:]
 
