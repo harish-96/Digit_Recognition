aadc837804be5aa4b6c20c7b953e744184efdf8a
Harish <harish2111996@gmail.com>
2016-11-14 04:19:34 +0530

    Added template class for Swarm
    Added the init function and update_gbest which updates the glpbal best
    position of the swarm

--- pso.py
+++ pso.py
@@ -21,3 +21,28 @@ class Particle:
     def update_pbest(self, error_function):
         if error_function(self.position) < error_function(self.pbest):
             self.pbest = self.position
+
+
+class Swarm(Particle):
+
+    def __init__(self, numParticles, error_function, pdeath=0.005,
+                 dimension=[784 * 15, 15 * 10, 15, 10], w=0.729, c1=1.49445,
+                 c2=1.49445, exit_error=0.1):
+        self.dimension = dimension
+        self.exit_error = exit_error
+        self.error_function = error_function
+        self.swarm = np.asarray([Particle(self.dimension)
+                                 for i in range(numParticles)])
+        gbest_index = np.argmin([self.error_function(self.swarm[i].position)
+                                 for i in range(numParticles)])
+        self.gbest = self.swarm[gbest_index].position
+        self.numParticles = numParticles
+        self.w = w
+        self.c1 = c1
+        self.c2 = c2
+        self.pdeath = pdeath
+
+    def update_gbest(self):
+        gbest_index = np.argmin([self.error_function(self.swarm[i].position)
+                                 for i in range(self.numParticles)])
+        self.gbest = self.swarm[gbest_index].position
