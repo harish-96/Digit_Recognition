f6e0ef8e3ef6e6c482623a4c6cfd88b305fd2031
Harish <harish2111996@gmail.com>
2016-11-14 04:27:20 +0530

    Added comments for the methods and the docstrings for class members

--- pso.py
+++ pso.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 class Particle:
-    """A single particle in the swarm"""
+    """A single particle in the swarm. Represents one possible solution
+    of the optimisation problem. Initialise with dimension of the design space
+    and bounds for the variables"""
 
     def __init__(self, dimension, X_max=5, X_min=-5):
         self.position = []
@@ -19,11 +21,19 @@ class Particle:
         self.pbest = self.position
 
     def update_pbest(self, error_function):
+        """Updates the best position of the particle in its own
+        personal history"""
         if error_function(self.position) < error_function(self.pbest):
             self.pbest = self.position
 
 
 class Swarm(Particle):
+    """A swarm of particles which move around to find the optimum in the design
+    space. Initialise with number of particles in the swarm, error function to
+    compute fitness. Probability of death of particles - which if higher leads
+    to more exploration and exploitation, the parameters of PSO - w, c1 and c2
+    and exit error which is the error value at which the optimisation
+    procedure concludes"""
 
     def __init__(self, numParticles, error_function, pdeath=0.005,
                  dimension=[784 * 15, 15 * 10, 15, 10], w=0.729, c1=1.49445,
@@ -43,6 +53,8 @@ class Swarm(Particle):
         self.pdeath = pdeath
 
     def update_gbest(self):
+        """Update the current best position of the swarm based on the fitness
+        function defined in the init function"""
         gbest_index = np.argmin([self.error_function(self.swarm[i].position)
                                  for i in range(self.numParticles)])
         self.gbest = self.swarm[gbest_index].position
